efb1d6dd63ca304eb5b07bb5e751d63cd3f69e00
gveyrat <79520562+gveyrat@users.noreply.github.com>
2021-04-27 21:54:18 +0200

    title : OK, fontsizetitle : OK
    appened the element into the parent section (for future overflow) need to understand why it doesn't show as expected, going to bed of the büque

--- ditor/ditor.js
+++ ditor/ditor.js
@@ -1,46 +1,49 @@
 // TODO : pouvoir changer le texte après / avant de changer la couleur et que rien ne s'écrase, solution dans le lien ci-dessous
 // https://editor.p5js.org/yining/sketches/SyzbgJ0C
 
-let input, button, firstInputTitle;
-let titleColorSlider;
+// const : label (label), indic (value)
+
+let labelInputTitle, labelSliderSize, input, firstInputTitle, posterTitle, titleFontSizeIndic;
 
   function setup() {
+    let menuElt = document.getElementById('parent');
     createCanvas(450, 630);
 
-    input = createInput().attribute('placeholder', 'Poster title');
-    input.position(25,50);
-    input.id('posterTitle');
+    labelInputTitle = createSpan('Title : ');
+    labelInputTitle.position(25, 55);
+    labelInputTitle.addClass('labelSmall');
+    labelInputTitle.parent(menuElt);
 
-    button = createButton('submit');
-    button.position(input.x + input.width + 5, 50);
-    button.mousePressed(applyTitleOnPoster);
+    input = createInput().attribute('placeholder', 'Poster title');
+    input.position(labelInputTitle.width + 30, 50);
+    input.style('width', '250px');
+    input.parent(menuElt);
 
     firstInputTitle = createElement('h3', 'Poster\'s name options');
     firstInputTitle.position(25, 5);
+    firstInputTitle.parent(menuElt);
 
-    bgColorSlider = createSlider(0, 255);
-    bgColorSlider.position(input.x + input.width + 5, 75);
-    bgColorSlider.style('width', '80px');
-    bgColorSlider.mousePressed(changeTitleSize)
+    labelSliderSize = createSpan('Size : ');
+    labelSliderSize.position(25, 80);
+    labelSliderSize.addClass('labelSmall');
+    labelSliderSize.parent(menuElt);
 
-    background(0);
+    titleSizeSlider = createSlider(12, 50);
+    titleSizeSlider.position(labelSliderSize.width + 30, 75);
+    titleSizeSlider.style('width', '80px');
+    titleSizeSlider.parent(menuElt);
+
+    titleFontSizeIndic = createSpan();
+    titleFontSizeIndic.position(titleSizeSlider.x + titleSizeSlider.width + 15, 77);
+    titleFontSizeIndic.parent(menuElt);
+
+    background(20);
   }
 
-  function changeTitleSize() {
-    let val = bgColorSlider.value();
-    background(val);
-  }
-
-  function applyTitleOnPoster(val) {
-    background(val);
-    const title = input.value();
-    console.log(title)
-    input.value('');
-
-    for (let i = 0; i < 50; i += 10) {
-      push();
-      fill(255);
-      text (title, width/2, i + 50);
-      pop();
-    }
+  function draw() {
+    background(20);
+    fill(255);
+    textSize(titleSizeSlider.value());
+    titleFontSizeIndic.html(titleSizeSlider.value() + "px");
+    text(input.value(), 0 + 30, height - 30);
   }
